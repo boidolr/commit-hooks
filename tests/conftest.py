8e0be42816c8827d1e5f0396a9894fcff72ebb57
Raphael Boidol <boidolr@users.noreply.github.com>
2020-06-20 19:55:41 +0200

    Add test (and example) of dictionary

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import pathlib
 import pytest
 
 from hooks.command_util import _execute_command
@@ -8,3 +9,9 @@ def temp_git_dir(tmpdir):
     git_dir = tmpdir.join('gits')
     _execute_command('git', 'init', '--', git_dir.strpath)
     yield git_dir
+
+
+@pytest.fixture
+def dictionary_path():
+    current_file = pathlib.Path(__file__).resolve()
+    yield str(current_file.parent / 'words.json')
